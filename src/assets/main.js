e0ef724af9d002413d45b5d2f7954a63fc06a134
Petri Behm <petri.behm@paf.com>
2018-02-15 17:11:00 +0200

    Add new UI for badges.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,22 @@
 $(function() {
 
-  // your code will go here
+	$.ajax({
+		url: 'https://www.codeschool.com/users/pepevara.json',
+		dataType: 'jsonp',
+		success: function(response) {
+			let arr = response.courses.completed;
+			$.each(arr, function(i, item)
+			{
+				let html =
+				"<div class='course'>" +
+					"<h3>" + item.title + "</h3>" +
+					"<img src='"+ item.badge +"' alt='" + item.title + "' />" +
+					"<a class='btn btn-primary' target='_blank' href='" + item.url + "'>See Course</a>"
+				"</div>";
+
+				$("#badges").append( html );
+			});
+		}
+	});
 
 });
